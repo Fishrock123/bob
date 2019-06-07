6ade1d403da48499b8afe8254ab442a042d91437
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2019-06-07 15:31:56 -0700

    helpers: make Stream-s be able to consume Stream-s

--- helpers/stream.js
+++ helpers/stream.js
@@ -1,52 +1,82 @@
 'use strict'
 
 class Stream {
-  #source = null
-  #sink = null
-  #promise = null
-  #resolve = null
-  #reject = null
+  source = null
+  sink = null
+  promise = null
+  resolve = null
+  reject = null
+  bindCb = null
 
-  constructor(source, ...sinks) {
-    this.#source = source
-    this.#sink = sinks[sinks.length - 1]
+  constructor(...components) {
+    const last = components.length - 1
+    const source = this.source = components[0]
+    const sink = this.sink = components[last]
 
-    let last = source
-    for (const sink of sinks.slice(0, sinks.length - 1)) {
-      sink.bindSource(last)
-      last = sink
+    let above = source // Above the next sink in the flow
+    for (const intermediate of components.slice(1, last)) {
+      above = intermediate.bindSource(above)
     }
 
-    this.#sink.bindSource(last, error => {
-      if (this.#promise === null) {
-        throw error
-      }
-      if (error) {
-        this.#reject(error)
-      } else {
-        this.#resolve()
-      }
-    })
+    if (typeof sink.pull !== 'function') {
+      sink.bindSource(above, error => {
+        if (typeof this.bindCb === 'function') {
+          this.bindCB(error)
+        }
+
+        if (this.promise === null) {
+          if (error) throw error
+          else return
+        }
+        if (error) {
+          this.reject(error)
+        } else {
+          this.resolve()
+        }
+      })
+    } else {
+      sink.bindSource(above)
+    }
   }
 
   start() {
     // If sink is undefined or does not have start(), a programmer error has been made.
-    return this.sink.start()
+    this.sink.start()
+    return this
   }
 
   stop() {
     // If source is undefined or does not have stop(), a programmer error has been made.
-    return this.source.stop()
+    this.source.stop()
+    return this
   }
 
   then(_resolve, _reject) {
-    if (!this.#promise) {
-      this.#promise = new Promise((resolve, reject) => {
-        this.#resolve = resolve
-        this.#reject = reject
+    if (!this.promise) {
+      this.promise = new Promise((resolve, reject) => {
+        this.resolve = resolve
+        this.reject = reject
       })
     }
-    return this.#promise.then(_resolve, _reject)
+    return this.promise.then(_resolve, _reject)
+  }
+
+  bindSource (source, bindCb) {
+    if (this.bindCb) throw new Error('Already bound Stream')
+    this.bindCb = bindCb
+    return this.source.bindSource(source)
+  }
+
+  bindSink (sink) {
+    return this.sink.bindSink(sink)
+  }
+
+  next (status, error, buffer, bytes) {
+    this.source.next(status, error, buffer, bytes)
+  }
+
+  pull (error, buffer) {
+    this.sink.pull(error, buffer)
   }
 }
 
